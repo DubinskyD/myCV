8c900e6c89505888921292c37cf60c2cd9e545a9
Constantine Simm <simm.ufo@gmail.com>
2021-07-06 15:42:43 +0300

    added cors

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.static(join(__dirname, 'build')));
 
+if (process.env.NODE_ENV !== 'production') {
+  server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next();
+  })
+}
 server.use('/cv', routes);
 
 if (process.env.NODE_ENV === 'production') {
