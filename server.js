8e6ea95216ef0390a23c27c2ff9c75293cbdde80
Constantine Simm <simm.ufo@gmail.com>
2021-07-06 16:20:37 +0300

    added powered by

--- server.js
+++ server.js
@@ -8,16 +8,12 @@ const routes = require('./routes/cv');
 server
     .use(express.json())
     .use(express.urlencoded({ extended: false }))
-    .use(express.static(join(__dirname, 'build')));
+    .use(express.static(join(__dirname, 'build')))
+    .use((req, res, next) => {
+      res.header('X-Powered-By', 'Dmitriy Dubinsky <Dmitriy.Dubinsky@gmail.com>');
 
-if (process.env.NODE_ENV === 'production') {
-  server.use((req, res, next) => {
-    res.header('X-Powered-By', 'Dmitriy Dubinsky <Dmitriy.Dubinsky@gmail.com>');
-
-    if (req.header('x-forwarded-proto') !== 'https') res.redirect(`https://${req.header('host')}`)
-    else  next();
-  })
-}
+      next();
+    })
 
 if (process.env.NODE_ENV !== 'production') {
   server
