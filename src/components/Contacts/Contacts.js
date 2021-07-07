e105179ab9eb864df90ba839367ae2462970a37f
DubinskyD <Dmitriy.Dubinsky@gmail.com>
2021-07-07 12:59:18 +0300

    add some styles

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import styles from './contacts.module.css';
 
 const Contacts = ({ contacts }) => {
    return (
-      <div>
-         {contacts.telephone_number}
+      <div className={styles.wrapper}>
+
+         <a href={contacts.email} className={styles.mail}>{contacts.email}</a>
+         <a href={contacts.facebook} className={styles.facebook}>{contacts.facebook}</a>
+         <a href={contacts.linkedin} className={styles.linkedin}>{contacts.linkedin}</a>
+         <a href={contacts.github} className={styles.github}>{contacts.github}</a>
+         <a href={contacts.skype} className={styles.skype}>{contacts.skype}</a>
+         <a href={contacts.telephone_number} className={styles.number}>{contacts.telephone_number}</a>
+         <a href={contacts.telephone_number} className={styles.telegram}>telegram</a>
+         <a href={contacts.telephone_number} className={styles.viber}>viber</a>
+         <span className={styles.location}>{contacts.location}</span>
       </div>
    )
 }
--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -1,24 +1,24 @@
 import React from 'react';
-
+import styles from './workExperience.module.css';
 
 const WorkExperience = ({ work_experience }) => {
 
 
    return (
-      <div>
-         <h3>Work Experience</h3>
+      <div className={styles.wrapper}>
+         <h3 className={styles.title}>Work Experience</h3>
          {work_experience.map(place => (
-            <div key={place}>
-               <p>{place.position}</p>
-               <p>{place.department}</p>
-               <p>
-                  <span>{place.period},</span><span>{place.company}</span>
+            <div className={styles.place} key={place}>
+               <p className={styles.position}>{place.position}</p>
+               <p className={styles.department}>{place.department}</p>
+               <p className={styles.periodWrapper} >
+                  <span>{place.period}, </span><span>{place.company}</span>
                </p>
-               <p>{place.direction}</p>
-               <ul>
+               <p className={styles.direction}>{place.direction}</p>
+               <ul className={styles.list}>
                   {place.responsibilities.map(resp => (
 
-                     <li key={resp} > {resp}</li>
+                     <li className={styles.listResp} key={resp} > {resp}</li>
                   ))}
                </ul>
             </div>
