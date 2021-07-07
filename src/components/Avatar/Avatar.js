02e619545f99d1f0f0ba42f7b4914343e1ae7f22
DubinskyD <Dmitriy.Dubinsky@gmail.com>
2021-07-07 15:27:31 +0300

    add contacts block

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,7 +4,7 @@ import style from './avatar.module.css';
 const Avatar = ({ name, surname, grade, position }) => {
    return (
       <div className={style.avatarContainer}>
-         <img src='../../assets/photo/photo1.jpg' className={style.avatar}></img>
+         <img src='../../assets/photo/photo1.jpg' alt='#' className={style.avatar}></img>
          <p className={style.fullName}><span>{name} </span><span>{surname}</span></p>
          <p className={style.position}><span>{grade} </span><span>{position}</span></p>
       </div>
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import styles from './contacts.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faGithubSquare, faLinkedin, faSkype, faTelegram, faViber } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faCompass } from "@fortawesome/free-regular-svg-icons";
 
 const Contacts = ({ contacts }) => {
    return (
       <div className={styles.wrapper}>
+         <a href={`mailto:` + contacts.email} className={styles.emailWrapper}>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span className={styles.email}> {contacts.email}</span>
+         </a>
+         <div className={styles.socialMediaWrapper}>
+            <a href={contacts.facebook} className={styles.facebook}>
+               <FontAwesomeIcon icon={faFacebookSquare} />
+            </a>
 
-         <a href={contacts.email} className={styles.mail}>{contacts.email}</a>
-         <a href={contacts.facebook} className={styles.facebook}>{contacts.facebook}</a>
-         <a href={contacts.linkedin} className={styles.linkedin}>{contacts.linkedin}</a>
-         <a href={contacts.github} className={styles.github}>{contacts.github}</a>
-         <a href={contacts.skype} className={styles.skype}>{contacts.skype}</a>
-         <a href={contacts.telephone_number} className={styles.number}>{contacts.telephone_number}</a>
-         <a href={contacts.telephone_number} className={styles.telegram}>telegram</a>
-         <a href={contacts.telephone_number} className={styles.viber}>viber</a>
-         <span className={styles.location}>{contacts.location}</span>
+            <a href={contacts.linkedin} className={styles.linkedin}>
+               <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href={contacts.github} className={styles.github}>
+               <FontAwesomeIcon icon={faGithubSquare} />
+            </a>
+            <a href={contacts.skype} className={styles.skype}>
+               <FontAwesomeIcon icon={faSkype} />
+            </a>
+         </div>
+         <div className={styles.phoneWrapper}>
+            <a href={`tg://resolve?domain=` + contacts.telephone_number} className={styles.telegram}>
+               <FontAwesomeIcon icon={faTelegram} />
+            </a>
+            <a href={`viber://add?number=` + contacts.telephone_number} className={styles.viber}>
+               <FontAwesomeIcon icon={faViber} />
+            </a>
+            <a href={'tel:' + contacts.telephone_number} className={styles.number}>
+               {contacts.telephone_number}
+            </a>
+
+         </div>
+
+         <div className={styles.locationWrapper}>
+            <FontAwesomeIcon icon={faCompass} />
+            <span className={styles.location}>{contacts.location}</span>
+         </div>
+
       </div>
    )
 }
